81d66ceef857d595bdb25d6862391af023ee13b9
thh06 <kikutatuki@gmail.com>
2024-10-21 07:13:20 +0000

    rails generate controller StaticPages home help

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,12 @@
 class StaticPagesController < ApplicationController
   def home
+    # render ...
+    # => app/views/static_pages/home.html.erb
   end
 
   def help
+    # => app/views/statics_pages/help/html.erb
+    # roots / root / rooting
+    # routes / route / routing
   end
 end
