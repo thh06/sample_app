f21bae2b3fb4a3058ad07f54a03e979208902819
thh06 <kikutatuki@gmail.com>
2024-11-22 08:45:31 +0000

    Finish ch05

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -13,4 +13,8 @@ class StaticPagesController < ApplicationController
   def about
     # => app/views/static_pages/about.html.erb
   end
+
+  def contact
+    # => app/views/static_pages/contact.html.erb
+  end
 end
